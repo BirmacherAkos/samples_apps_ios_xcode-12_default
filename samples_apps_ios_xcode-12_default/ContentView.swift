32d6488a32336fe27d4eeda87117dc5e423b4d6c
Akos Birmacher <akos.birmacher@bitrise.io>
2021-02-09 11:18:56 +0100

    login button added

--- samples_apps_ios_xcode-12_default/ContentView.swift
+++ samples_apps_ios_xcode-12_default/ContentView.swift
@@ -11,11 +11,25 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+        Button(action: {
+            print("Login tapped!")
+        }) {
+            HStack {
+                Text("Login")
+                    .fontWeight(.semibold)
+                    .font(.title)
+            }
+        }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
